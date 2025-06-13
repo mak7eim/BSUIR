24da9742a54b534262dc48603e0e228434514a23
mak7eim <max.pratko@gmail.com>
2025-06-13 23:26:44 +0300

    Add files via upload

---
+++ Programming C#/lab1/_453503_Пратько/_453503_Пратько/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _453503_Пратько
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2;
+
+            Console.WriteLine("Введите первое число: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = num1 / num2;
+
+            Console.WriteLine("Частное от деления: " + result);
+        }
+    }
+}
